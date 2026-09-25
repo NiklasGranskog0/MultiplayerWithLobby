00f183b2666748904a2720037a9ecdd17e1acacf
Niklas Granskog <granen.n@gmail.com>
2026-09-25 19:35:35 +0200

    waowie

--- Assets/Project_Assets/Scripts/Units/UnitBase.cs
+++ Assets/Project_Assets/Scripts/Units/UnitBase.cs
@@ -1,6 +1,5 @@
 using Project_Assets.Scripts.Enums;
 using Project_Assets.Scripts.Framework.UnityServiceLocator;
-using Project_Assets.Scripts.Game;
 using Project_Assets.Scripts.Game.MenuButtons;
 using Project_Assets.Scripts.Interfaces;
 using Unity.Collections;
